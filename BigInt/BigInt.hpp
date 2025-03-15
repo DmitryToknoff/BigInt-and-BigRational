2ae711d9780a607a8e29491dacdd14b6bdb239a6
Dmitry Toknov <toknoff@yandex.ru>
2025-03-16 00:13:31 +0300

    add gitignore

--- BigInt/BigInt.hpp
+++ BigInt/BigInt.hpp
@@ -1,20 +1,50 @@
-//
-//  BigInt.hpp
-//  BigInt
-//
-//  Created by Дмитрий Токнов on 16.03.2025.
-//
+#include <iostream>
+#include <string>
 
 #ifndef BigInt_
 #define BigInt_
 
-/* The classes below are exported */
+
 #pragma GCC visibility push(default)
+#pragma once
 
 class BigInt
 {
-    public:
-    void HelloWorld(const char *);
+    char* nums;
+    size_t size = 0;
+    bool sign = true;
+    
+    
+    size_t get_size(const char* s);
+    
+public:
+    BigInt();
+    BigInt(const BigInt& bi);
+    BigInt(long long num);
+    BigInt(const char* s);
+    
+    ~BigInt();
+    
+    BigInt& operator= (const BigInt& bi);
+    BigInt& operator *= (const BigInt& bi);
+    BigInt& operator /= (const BigInt& bi);
+    BigInt& operator += (const BigInt& bi);
+    BigInt& operator -= (const BigInt& bi);
+    BigInt operator * (const BigInt& bi);
+    BigInt operator / (const BigInt& bi);
+    BigInt operator - (const BigInt& bi);
+    BigInt operator + (const BigInt& bi);
+    BigInt& operator - ();
+    bool operator < (const BigInt& bi);
+    bool operator <= (const BigInt& bi);
+    bool operator == (const BigInt& bi);
+    
+    friend std::ostream& operator<< (std::ostream& os, const BigInt& bi);
+    friend std::istream& operator>> (std::istream& is, const BigInt& bi);
+    
+    void ToString();
+    
+    
 };
 
 #pragma GCC visibility pop
